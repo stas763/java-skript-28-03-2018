a51e166ef33da0758e2d6254e8287ed20d90a6c2
stan kleshnov <s.v.kleshnov@gmail.com>
2018-03-28 16:03:01 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -3,26 +3,49 @@
 // greet = 'Goodbye';
 // alert(greet);
 
+// // String
+// const someName = 'Jon';
+
+// // Number
+// const age = 42.5;
+
+// // Boolean
+// const married = true; // false
+
+// // undefined
+// //const notDefined = undefined;
+// let notDefined;  
+
+// // null
+// const empty = null;
+
+// // Object
+// const someObj = {};
+
+// // Symbol
+// const someSym = Symbol();
+
 const user = prompt('Enter your name');
-alert('Hello ' + user);
-console.log('Hello ' + user);
 document.write('Hello ' + user);
+document.write('<br>');
 
-// String
-const someName = 'Jon';
+const age = parseFloat(prompt('Enter your age'));
+const nextAge = addOne(age);
+document.write('You are ' + checkAge(age));
+document.write('Next year you will be ' + nextAge);
 
-// Number
-const age = 42.5;
 
-// Boolean
-const married = true; // false
+function addOne(arg) {
+    const result = arg + 1;
+    return result;
+}
 
-// undefined
-//const notDefined = undefined;
-let notDefined;  
-
-// null
-const empty = null;
-
-// Object
-const someObj = {};
+function checkAge(age) {
+    if ( (age >= 18) && (age <= 70) ){
+        return 'Adult';
+    } else if (age > 70) {
+        return 'Old';
+    } else {
+        return 'Child';
+    }
+}
