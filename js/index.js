db68116fdcec949269c01889f06acd1147363a9b
stan kleshnov <s.v.kleshnov@gmail.com>
2018-03-28 15:30:37 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// let greet = 'Hello World!';
+// alert(greet);
+// greet = 'Goodbye';
+// alert(greet);
+
+const user = prompt('Enter your name');
+alert('Hello ' + user);
+console.log('Hello ' + user);
+document.write('Hello ' + user);
+
+// String
+const someName = 'Jon';
+
+// Number
+const age = 42.5;
+
+// Boolean
+const married = true; // false
+
+// undefined
+//const notDefined = undefined;
+let notDefined;  
+
+// null
+const empty = null;
+
+// Object
+const someObj = {};
